8475af77af9f1d0bb65c1dfea94e2ec7cb079858
Hossein MJafari <ho3ein.mj.1@gmail.com>
2023-04-13 14:31:08 +0430

    Created all the class names and inheritences.

---
+++ Animal.py
@@ -0,0 +1,61 @@
+class Animal:
+    pass
+
+
+class Mamals(Animal):
+    pass
+class Dog(Mamals):
+    pass
+class Cat(Mamals):
+    pass
+class Horse(Mamals):
+    pass
+class Cow(Mamals):
+    pass
+class Elephant(Mamals):
+    pass
+
+
+class Birds(Animal):
+    pass
+class Eagle(Birds):
+    pass
+class Penguin(Birds):
+    pass
+class Parrot(Birds):
+    pass
+
+
+class Fish(Animal):
+    pass
+class Shark(Fish):
+    pass
+class Salmon(Fish):
+    pass
+
+
+class Reptiles(Animal):
+    pass
+class Snake(Reptiles):
+    pass
+class Lizard(Reptiles):
+    pass
+
+
+class Amphibian(Animal):
+    pass
+class Frog(Amphibian):
+    pass
+
+
+class Habitat:
+    pass
+
+
+class Food:
+    pass
+
+
+class CareTaker:
+    pass
+
