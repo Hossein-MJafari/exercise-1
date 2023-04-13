05854cff4a2faa95fdc20098074575ce17e4192f
Hossein MJafari <ho3ein.mj.1@gmail.com>
2023-04-13 22:40:02 +0430

    testing some functionalities + finishing the code.

--- Animal.py
+++ Animal.py
@@ -18,7 +18,7 @@ class Animal:
         print(f"{self.name} is eating {self.food.name}.")
 
     def communicate(self):
-        pass
+        return f"Hi...my name is {self.name}"
 
     def breed(self):
         pass
@@ -28,35 +28,37 @@ class Animal:
 
 
 class mammals(Animal):
-    def __init__(self, name, age, fur_color, warmblooded):
-        super().__init__(name, age)
+    def __init__(self, name, age, weight, height, species, fur_color, warmblooded:bool):
+        super().__init__(name, age, weight, height, species)
         self.fur_color = fur_color
         self.warmblooded = warmblooded
 
 
 class Dog(mammals):
-    def __init__(self, name, breed):
-        super().__init__(name)
+    def __init__(self, name, age, weight, height, species, breed):
+        super().__init__(name, age, weight, height, species)
         self.breed = breed
         self.sound = 'Woof'
 
     def bark(self):
+        super().communicate()
         return self.sound
 
 
 class Cat(mammals):
-    def __init__(self, name, breed):
-        super().__init__(name)
+    def __init__(self, name, age, weight, height, species, breed):
+        super().__init__(name, age, weight, height, species)
         self.breed = breed
         self.sound = 'Meow'
 
     def meow(self):
+        super().communicate()
         return self.sound
 
 
 class Horse(mammals):
-    def __init__(self, name, breed, manecolor):
-        super().__init__(name)
+    def __init__(self, name, age, weight, height, species, breed, manecolor):
+        super().__init__(name, age, weight, height, species)
         self.breed = breed
         self.manecolor = manecolor
 
@@ -65,28 +67,30 @@ class Horse(mammals):
 
 
 class Cow(mammals):
-    def __init__(self, name, breed, milkproduction):
-        super().__init__(name)
+    def __init__(self, name, age, weight, height, species, breed, milkproduction):
+        super().__init__(name, age, weight, height, species)
         self.breed = breed
         self.milkproduction = milkproduction
 
     def moo(self):
+        super().communicate()
         return 'Moooo...'
 
 
 class Elephant(mammals):
-    def __init__(self, name, tuskLength, trunkLength):
-        super().__init__(name)
+    def __init__(self, name, age, weight, height, species, tuskLength, trunkLength):
+        super().__init__(name, age, weight, height, species)
         self.tuskLength = tuskLength
         self.trunkLength = trunkLength
 
     def trumpet(self):
+        super().communicate()
         return f'{self.name} is making the trumpet sound.'
 
 
 class Birds(Animal):
-    def __init__(self, name, age, weight, height, featherColor, beakType):
-        super().__init__(name, age, weight, height)
+    def __init__(self, name, age, weight, height, species, featherColor, beakType):
+        super().__init__(name, age, weight, height, species)
         self.featherColor = featherColor
         self.beakType = beakType
 
@@ -96,94 +100,95 @@ class Birds(Animal):
 
 
 class Eagle(Birds):
-    def __init__(self, name, age, weight, height, featherColor, beakType, wingLength):
-        super().__init__(name, age, weight, height, featherColor, beakType)
+    def __init__(self, name, age, weight, height, species, featherColor, beakType, wingLength):
+        super().__init__(name, age, weight, height, species, featherColor, beakType)
         self.wingLength = wingLength
 
 
 class Penguin(Birds):
-    def __init__(self, name, age, weight, height, featherColor, beakType, swimSpeed):
-        super().__init__(name, age, weight, height, featherColor, beakType)
+    def __init__(self, name, age, weight, height, species, featherColor, beakType, swimSpeed):
+        super().__init__(name, age, weight, height, species, featherColor, beakType)
         self.swimSpeed = swimSpeed
-    
+
     def slideOnBelly(self):
         super().fly()
         return "Wwwoooeeeeiiiii, I'm sliding on my belly on ice!"
-    
-
 
 
 class Parrot(Birds):
-    def __init__(self, name, age, weight, height, featherColor, beakType):
-        super().__init__(name, age, weight, height, featherColor, beakType)
+    def __init__(self, name, age, weight, height, species, featherColor, beakType):
+        super().__init__(name, age, weight, height, species, featherColor, beakType)
 
     def mimicSound(self, sound):
         return f'{self.name} mimics {sound}.'
 
 
 class Fish(Animal):
-    def __init__(self, name, age, weight, height, coldBlooded, scaleColor):
-        super().__init__(name, age, weight, height)
+    def __init__(self, name, age, weight, height, species, coldBlooded:bool, scaleColor):
+        super().__init__(name, age, weight, height, species)
         self.coldBlooded = coldBlooded
         self.scaleColor = scaleColor
-    
+
     def swim(self):
         super().move()
         return 'Swimming...'
 
 
 class Shark(Fish):
-    def __init__(self, name, age, weight, height, coldBlooded, scaleColor):
-        super().__init__(name, age, weight, height, coldBlooded, scaleColor)
-    
+    def __init__(self, name, age, weight, height, species, coldBlooded, scaleColor):
+        super().__init__(name, age, weight, height, species, coldBlooded, scaleColor)
+
     def hunt(self):
         return f'{self.name} just hunted a fish!'
 
 
 class Salmon(Fish):
-    def __init__(self, name, age, weight, height, coldBlooded, scaleColor):
-        super().__init__(name, age, weight, height, coldBlooded, scaleColor)
+    def __init__(self, name, age, weight, height, species, coldBlooded, scaleColor):
+        super().__init__(name, age, weight, height, species, coldBlooded, scaleColor)
 
 
 class Reptiles(Animal):
-    def __init__(self, name, age, weight, color):
-        super().__init__(name, age, weight)
+    def __init__(self, name, age, weight, height, species, color):
+        super().__init__(name, age, weight, height, species)
         self.color = color
-    
+
     def shedSkin(self):
         return f'{self.name} is preparing its new skin!'
 
+
 class Snake(Reptiles):
-    def __init__(self, name, age, weight, color, venomous: bool):
-        super().__init__(name, age, weight, color)
+    def __init__(self, name, age, weight, height, species, color, venomous: bool):
+        super().__init__(name, age, weight, height, species, color)
         self.venomous = venomous
-    
+
     def slither(self):
         super().move()
         return f'{self.name} is slithering...'
 
+
 class Lizard(Reptiles):
-    def __init__(self, name, age, weight, color):
-        super().__init__(name, age, weight, color)
+    def __init__(self, name, age, weight, height, species, color):
+        super().__init__(name, age, weight, height, species, color)
 
     def regenerateTail(self):
         return f"ahh... {self.name}'s tail got cut again, regenerating..."
 
+
 class Amphibian(Animal):
-    def __init__(self, name, age, weight, skinType, metamorphisis:bool):
-        super().__init__(name, age, weight)
+    def __init__(self, name, age, weight, height, species, skinType, metamorphisis: bool):
+        super().__init__(name, age, weight, height, species,)
         self.skinType = skinType
         self.metamorphisis = metamorphisis
-    
+
     def breathThroughSkin(self):
         return 'Who needs lungs?...when you can breath through your skin...'
 
 
 class Frog(Amphibian):
-    def __init__(self, name, age, weight, skinType, metamorphisis: bool, sound):
-        super().__init__(name, age, weight, skinType, metamorphisis)
+    def __init__(self, name, age, weight, height, species, skinType, metamorphisis: bool, sound):
+        super().__init__(name, age, weight, height, species, skinType, metamorphisis)
         self.sound = sound
-    
+
     def jump(self):
         super().move()
         return f'{self.name} just jumped over there!'
@@ -252,8 +257,19 @@ savannah.set_food(leaves.name)
 # set food for animal
 giraffe = savannah.animals[0]
 giraffe.set_food(leaves.name)
+
 lion = jane.animals[0]
 lion.set_food(leaves.name)
 
 # caretaker feeds the animals
 jane.feed_animal(leaves.name)
+
+# another example
+sea = Habitat("Caspian-Sea", "Iran", "cool-humid")
+sea.add_animal('Mr.Shark', 5, 80, 1.8,'white-shark')
+sea.set_food("fishes")
+
+
+mrShark = Shark('Mr.Shark', 5, 80, 1.8,'white-shark', False, 'light-blue')
+print(mrShark.hunt())
+print(mrShark.communicate())
